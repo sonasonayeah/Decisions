53b0324671ed189ce538a7116ff14d0981d6655f
StuntPlayZYT <stuntnhoney@gmail.com>
2021-07-14 13:21:33 -0700

    Initial commit

---
+++ Decisions/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Decisions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         Console.WriteLine("Bob's Big Giveaway!");
+            Console.Write("Choose a door: 1, 2 or 3:");
+            string userValue = Console.ReadLine();
+
+            if (userValue == "1")
+            {
+                string message = "You won a new car!";
+                Console.WriteLine(message);
+            }
+            else if (userValue == "2")
+            {
+                string message = "You won a new boat!";
+                Console.WriteLine(message);
+            }
+            else if (userValue == "3")
+            {
+                string message = "You won a new cat!";
+                Console.WriteLine(message);
+            }
+            else
+            {
+                string message = "Sorry, we didn't understand.";
+                Console.WriteLine(message);
+            }
+           
+            Console.ReadLine();
+        }
+    }
+}
